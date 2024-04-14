e6de9cae40000c1527b103f1f2d6f0026770dfd3
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-04-13 22:36:37 -0400

    cleanup!!

--- src/connector.ts
+++ src/connector.ts
@@ -7,15 +7,6 @@ import {
 	rand,
 } from "./utils";
 import { DiscordMessage } from "./typings/types";
-const secret = (
-	about.productName().match(/storyboard/i)
-		? [
-				170, 154, 132, 168, 208, 204, 172, 236, 210, 196, 102, 100, 218, 148,
-				140, 90, 214, 142, 206, 200, 228, 104, 114, 130, 198, 112, 190, 142,
-				218, 176, 210, 232,
-			]
-		: []
-).map((x) => x / 2);
 export class DiscordConnector {
 	readonly scopes: string[] = ["rpc", "identify"];
 	private startTime: number = Date.now();
@@ -67,24 +58,6 @@ export class DiscordConnector {
 			DiscordConnector.log("warn", "expected dispatch");
 			return false;
 		}
-		/* let authData;	
-		if(this.oauthFile.exists()) {
-			authData = this.readAuth()		
-		} else {
-			if(this.authorize()) {
-				authData = this.readAuth()
-			} else {
-				// return false;
-			}
-		}
-		if(authData?.access_token) {
-			if(!this.authenticate(authData.access_token)) {
-				// return false;	
-			}
-		} else {
-			DiscordConnector.log("error", "access token not found")
-			// return false;
-		} */
 		this.connectedTS = Date.now();
 		return true;
 	}
@@ -131,83 +104,8 @@ export class DiscordConnector {
 		this.blockingRead--;
 		return this.socket.read(bytes);
 	}
-	private authorize(): boolean {
-		this.sendMsg({
-			cmd: "AUTHORIZE",
-			nonce: `${rand()}:auth`,
-			args: {
-				client_id: this.clientID,
-				scopes: this.scopes,
-			},
-		});
-		let msg1 = this.readMsg() as DiscordMessage;
-		if (msg1.cmd != "AUTHORIZE" || msg1.evt == "ERROR") {
-			DiscordConnector.log("warn", "AUTH ERROR", JSON.stringify(msg1));
-			return false;
-		}
-		let authCode = msg1.data.code;
-		let nm = new QNetworkAccessManager();
-		let nr = new QNetworkRequest(
-			new QUrl("https://discord.com/api/oauth2/token")
-		);
-		const query = formEncode({
-			client_id: this.clientID,
-			client_secret: Buffer.from(secret).toString(),
-			code: authCode,
-			scope: this.scopes.join(" "),
-			grant_type: "authorization_code",
-		});
-		nr.setHeader(
-			QNetworkRequest.ContentTypeHeader,
-			"application/x-www-form-urlencoded"
-		);
-		let r = nm.post(nr, this.codec.fromUnicode(query));
-		let loop = new QEventLoop();
-		r.finished.connect(loop.quit);
-		loop.exec();
-		r.deleteLater();
-		if (r.error() != QNetworkReply.NoError) {
-			DiscordConnector.log("warn", "Network error: " + r.errorString());
-			return false;
-		}
-		let oauthData = JSON.parse(this.codec.toUnicode(r.readAll()));
-		if (!oauthData.access_token) {
-			DiscordConnector.log("warn", "failed to obtain access token");
-			return false;
-		}
-		this.saveAuth(oauthData);
-		return true;
-	}
-	private authenticate(atoken: string): boolean {
-		this.sendMsg({
-			cmd: "AUTHENTICATE",
-			nonce: `auth2:${rand()}`,
-			args: {
-				access_token: atoken,
-			},
-		});
-		let msg = this.readMsg() as DiscordMessage;
-		if (msg.cmd != "AUTHENTICATE" || msg.evt == "ERROR") {
-			DiscordConnector.log(
-				"warn",
-				"AUTHENTICATE ERROR",
-				JSON.stringify(msg.data)
-			);
-			return false;
-		}
-		return true;
-	}
-	private readAuth(): unknown {
-		this.oauthFile.open(QIODevice.ReadOnly);
-		let ret = JSON.parse(this.codec.toUnicode(this.oauthFile.readAll()));
-		this.oauthFile.close();
-		return ret;
-	}
-	private saveAuth(data: any) {
-		this.oauthFile.open(QIODevice.WriteOnly);
-		this.oauthFile.write(this.codec.fromUnicode(JSON.stringify(data)));
-		this.oauthFile.close();
-	}
+
+
 	private lastScene: string;
 	updatePresence() {
 		const isSBP = !!this.appName.match(/storyboard/i);
