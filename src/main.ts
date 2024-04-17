909661440edffc6d7aed9ee3ca8cdde891079f2a
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-04-17 19:49:56 -0400

    connect timer to connector

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { DiscordConnector } from "./connector";
-function doMain():void {
-	const clientId = about.productName().match(/storyboard/i) ? "857353205762359318" : ""
-	let connector = new DiscordConnector(clientId, about.productName());
+function doMain(): void {
+	const clientId = about.productName().match(/storyboard/i)
+		? "857353205762359318"
+		: "1229238624982274069";
+
 	let timer = new QTimer();
 	timer.interval = 1000;
 	timer.singleshot = false;
-	timer.timeout.connect(connector, connector.updatePresence);
-	timer.start()
+
+	this.connector = new DiscordConnector(clientId, about.productName(), timer);
+	this.connector.timer.start()
 }
-doMain()
+doMain();
